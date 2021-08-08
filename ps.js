6afae681d9c74691540873342a18ce17c024e4ef
mdriyadmr968 <mdriyadmr968@gmail.com>
2021-08-09 00:32:40 +0600

    first commit

---
+++ ps.js
@@ -0,0 +1,67 @@
+// problem 01: seerToMon 
+
+// function seerToMon(seer) {
+//     //Exception Handling
+//     if (typeof seer != 'number'){
+//         return 'Please provide a number!';
+//     }
+//     if (seer < 0){
+//         return 'Please provide a positive number'
+//     }
+//     //calculation
+//     var convert = seer / 40;
+//     return convert;
+    
+// }
+// var result = seerToMon(80);
+// console.log(result);
+
+
+
+// problem 02: totalSales
+
+// var shirt = 100;
+// var pant = 200;
+// var shoe = 500;
+
+// function totalSales(shirtquantity, pantquantity, shoequantity) {
+//     // Exeption handling
+//     if (typeof shirtquantity != 'number' || shirtquantity < 0) {
+//         return 'please provide a number';        
+//     }
+//     if (typeof pantquantity != 'number' || pantquantity < 0) {
+//         return 'please provide a number';        
+//     }
+//     if (typeof shoequantity != 'number' || shoequantity < 0) {
+//         return 'please provide a number';        
+//     }
+    
+    
+    
+//     var shirtprice = shirt * shirtquantity;
+//     var pantprice = pant *  pantquantity;
+//     var shoeprice = shoe * shoequantity;
+//     var totalprice = shirtprice+pantprice+shoeprice;
+//     return totalprice;
+    
+// }
+
+// result = totalSales(3, -5, 1);
+// console.log(result);
+
+
+
+
+
+
+// problem 03: deliveryCost 
+
+
+
+
+
+
+
+
+// problem 04: perfectFriend  
+
