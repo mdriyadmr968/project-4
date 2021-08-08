4e1c745007180126bc02deb3a4a589d67376d98b
mdriyadmr968 <mdriyadmr968@gmail.com>
2021-08-09 01:22:45 +0600

    second commit

--- ps.js
+++ ps.js
@@ -20,11 +20,11 @@
 
 // problem 02: totalSales
 
-// var shirt = 100;
-// var pant = 200;
-// var shoe = 500;
-
 // function totalSales(shirtquantity, pantquantity, shoequantity) {
+// variable declaration 
+//     const shirt = 100;
+//     const pant = 200;
+//     const shoe = 500;
 //     // Exeption handling
 //     if (typeof shirtquantity != 'number' || shirtquantity < 0) {
 //         return 'please provide a number';        
@@ -46,7 +46,7 @@
     
 // }
 
-// result = totalSales(3, -5, 1);
+// result = totalSales(3, 5, 1);
 // console.log(result);
 
 
@@ -55,6 +55,48 @@
 
 
 // problem 03: deliveryCost 
+// function deliveryCost(shirt) {
+//     //exception handling
+//     if(shirt <= 0){
+//         return 'please enter a number';
+//     }
+//     if (typeof shirt != 'number'){
+//         return 'Please provide a number!';
+//     }
+    
+//     // variable declaration 
+//     const shirtCostUnderfirst100 = 100;
+//     const shirtCostUndersecond100 = 80;
+//     const shirtCostUnderthird100 = 50;
+    
+
+//     // deliverycost calculation 
+//     if(shirt <= 100){
+//         var totalcost = shirt * shirtCostUnderfirst100;
+//         return totalcost;
+//     }
+//     else if (shirt <=200){
+//         const first100shirt = 100 * shirtCostUnderfirst100 ;
+//         const restshirt = shirt - 100;
+//         const second100shirt = restshirt * shirtCostUndersecond100;
+//         const totalcost = first100shirt+second100shirt;
+//         return totalcost;
+        
+//     }
+//     else{
+//         const first100shirt = 100 * shirtCostUnderfirst100 ;
+//         const second100shirt = 100 * shirtCostUndersecond100 ;
+//         const restshirt = shirt - 200;
+//         const third100shirt =restshirt * shirtCostUnderthird100;
+//         const totalcost = first100shirt+ second100shirt+ third100shirt;
+//         return totalcost;
+
+        
+//     }
+// }
+
+// const result = deliveryCost(201);
+// console.log(result);
 
 
 
